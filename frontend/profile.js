31294c8d2b9c253a704218e63daebafd1b589074
Baker Khoder <bakerkhoder878@gmail.com>
2022-09-16 00:17:50 +0300

    fetching data method for the post

--- frontend/profile.js
+++ frontend/profile.js
@@ -4,10 +4,6 @@ const popup=document.getElementById("popup")
 const darkenmask=document.getElementById("page-mask")
 const rightside=document.getElementById("rightside")
 const leftside=document.getElementById("leftside")
-const signin=document.getElementById("signin")
-const date=document.getElementById("date")
-const email=document.getElementById("email")
-const password=document.getElementById("password")
 
 pressed=false
 console.log("ji")
@@ -39,3 +35,70 @@ console.log("ji")
   })   
  }) */
 })
+
+
+
+/*const display=()=>{
+  if(tweetcontent){
+
+  }
+}*/
+
+fetch('http://localhost/php-contact/backend/contacts.php')
+
+  .then((response) => response.json())
+  .then((data) => {data.forEach((tweet)=>{
+    console.log(tweet)
+     let displaytweet = document.querySelector(".display-tweet")
+     let card = document.createElement("div")
+     card.classList.add("tweet-card")
+     displaytweet.appendChild(card)
+     let tweettop=document.createElement("div")
+     card.appendChild(tweettop)
+     tweettop.classList.add("tweet-card-top")
+     let profimage=document.createElement("img")
+     tweettop.appendChild(profimage)
+     profimage.classList.add("profile-image")
+     profimage.src="./images/3.png"
+     let usercontent=document.createElement("div")
+     tweettop.appendChild(usercontent)
+     usercontent.classList.add("user-and-content")
+     let usertweet=document.createElement("div")
+     usercontent.appendChild(usertweet)
+     usertweet.classList.add("user-tweet")
+     let username=document.createElement("div")
+     usertweet.appendChild(username)
+     username.textContent="username"
+     let email=document.createElement("div")
+     usertweet.appendChild(email)
+     email.textContent="email"
+     let tweettext=document.createElement("div")
+     usercontent.appendChild(tweettext)
+     tweettext.classList.add("tweet-text")
+     tweettext.textContent="buhbu"
+     //console.log("donne") fik tzid hon logo
+     let tweetimage=document.createElement("div")
+     card.appendChild(tweetimage)
+     tweetimage.classList.add("tweet-container-image")
+     let image=document.createElement("img")
+     tweetimage.appendChild(image)
+     image.classList.add("tweet-container-image")
+     image.src="./images/3.png"
+     let likebutton=document.createElement("div")
+     card.appendChild(likebutton)
+     likebutton.classList.add("like-button")
+     let likee=document.createElement("image")
+     likee.classList.add("like")
+     likebutton.appendChild(likee)
+     let nbr=document.createElement("div")
+     likebutton.appendChild(nbr)
+     nbr.textContent="7"
+
+
+
+
+
+
+
+
+})})
