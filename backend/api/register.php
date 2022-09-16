04ed2a2edf3e389905d0fb48d53ca0f88e25ae75
abbassmoussawi <abbass.y.moussawi@gmail.com>
2022-09-16 08:19:51 +0300

    use password_hach() in order to  hash the password

--- backend/api/register.php
+++ backend/api/register.php
@@ -5,7 +5,9 @@ $firstName = $_POST['first_name'];
 $lastName = $_POST['last_name'];
 $dob = $_POST['dob'];
 $email = $_POST['email'];
-$password = $_POST['passsword'];
+
+/* hashing the password*/
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
 $query = $mysqli->prepare("INSERT INTO users(first_name,last_name,dob,email, password) VALUES (?, ?, ? ,?,?)");
 $query ->bind_param('sssss',$firstName, $lastName,$dob, $email, $password);
