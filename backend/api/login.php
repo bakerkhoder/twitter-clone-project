cd6b862d7644c256436bd6554098eb0a64aaac4b
abbassmoussawi <abbass.y.moussawi@gmail.com>
2022-09-17 21:03:44 +0300

    add more comments

--- backend/api/login.php
+++ backend/api/login.php
@@ -1,39 +1,33 @@
 <?php
 require('../db/connection.php');
-// require('functions.php');
+require('functions.php');
 
 $email =  $_POST['email'];
 $password = $_POST['password'];
 
+//create a query for login
+$query = $mysqli->prepare("SELECT * FROM users WHERE email = ? limit 1");
+$query->bind_param('s', $email);
+$query->execute();
+//create a variable array  to store the results
+$array = $query->get_result();
 
-        $query = $mysqli->prepare("SELECT * FROM users WHERE email = ? limit 1");
-        $query->bind_param('s', $email);
-        $query->execute();
-        
-        $array = $query->get_result();
+$response = [];
 
-        $response = [];
+while ($a = $array->fetch_assoc()) {
+    $response[] = $a;
+}
 
-        while ($a = $array->fetch_assoc()) {
-            $response[] = $a;
-        }
+if (empty($response)) {
+    echo "operation failed";
+}
 
-        if(empty($response))
-        {
-            echo"operation failed";
-        }
+$user = $response[0];
 
-        $user = $response[0];
-     
-
-        if(password_verify($password, $user['password']))
-        {
-           echo"login successed";
-
-        }
-        echo"login failed";
-
-        // ma mesh lhal
-
-        $json = json_encode($response);
-        echo $json;
+//use password_verify() to check hshed password in database
+if (password_verify($password, $user['password'])) {
+    echo "login successed";
+}
+echo "login failed";
+$json = json_encode($response);
+echo $json;
