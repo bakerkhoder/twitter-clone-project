d51c19b48487a90fde53ca518bdfc1dfbef433d3
abbassmoussawi <abbass.y.moussawi@gmail.com>
2022-09-17 12:28:21 +0300

    create a query to count the number of likers for a specific tweet

--- backend/api/tweet_specifications.php
+++ backend/api/tweet_specifications.php
@@ -1,14 +1,14 @@
 <?php
 include("../db/connection.php");
 
-//select the number of users who like a tweet
-//select the owner of a specific tweet
-$query = $mysqli->prepare("SELECT count(likes_tweets.user_id) ,users.id, users.first_name FROM  likes_tweets
+//this query gives the count of users who like a specific tweet 
+//in addition to the name of its owner
+
+$query = $mysqli->prepare("SELECT count(likes_tweets.user_id), users.id, users.username FROM likes_tweets
 join users
 on users.id = likes_tweets.user_id
 WHERE likes_tweets.tweets_id = 2");
 
-//get the name of the
 $query->execute();
 $array = $query->get_result();
 
