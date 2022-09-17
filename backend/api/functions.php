060a816d63d8d6866bae2eefae332aca5ce581a8
abbassmoussawi <abbass.y.moussawi@gmail.com>
2022-09-17 13:55:54 +0300

    create the first function to sanitize the data

--- backend/api/functions.php
+++ backend/api/functions.php
@@ -1,5 +1,7 @@
 <?php
+//this function removes all the free spaces and special characters
 function check_input($data){
+    
     $data = htmlspecialchars($data);
     $data = trim($data);
     return $data;
