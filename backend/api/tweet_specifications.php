8eb596b2395af891e5fbcbe56066cd42f56ed309
abbassmoussawi <abbass.y.moussawi@gmail.com>
2022-09-17 13:39:37 +0300

    modify the query to get the data from the url

--- backend/api/tweet_specifications.php
+++ backend/api/tweet_specifications.php
@@ -1,12 +1,13 @@
 <?php
 include("../db/connection.php");
-
+$id = $_GET['id'];
 //this query gives the id, content, name and  username to a specific tweet 
 $query = $mysqli->prepare("
     SELECT tweet_id, tweets.content, tweets.user_id, users.first_name, users.username
     FROM `likes_tweets`
     join tweets on tweets.id = likes_tweets.tweet_id
-    join users on users.id = tweets.user_id;");
+    join users on users.id = tweets.user_id
+    where tweet_id =". $id ." limit 1");
 
 $query->execute();
 $array = $query->get_result();
