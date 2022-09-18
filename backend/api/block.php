37c20c10d445129e70a9cca73703a69270982089
abbassmoussawi <abbass.y.moussawi@gmail.com>
2022-09-18 21:01:27 +0300

    change  the parameterinto  integer instead of string

--- backend/api/block.php
+++ backend/api/block.php
@@ -5,10 +5,10 @@ require('functions.php');
 $user_blocked = $_POST['user_blocked'];
 $user_blocker = $_POST['user_blocker'];
 $query = $mysqli->prepare("INSERT INTO users_block(user_blocked,user_blocker) VALUES (?,?)");
-$query->bind_param('ss', $user_blocked, $user_blocker);
+$query->bind_param('ii', $user_blocked, $user_blocker);
 
 $query->execute();
 $response = [
-    "success"=>true
-]; 
+    "success" => true
+];
 echo json_encode($response);
