3f3069f34daae9d57dc4f79af27a93a37c4e9643
abbassmoussawi <abbass.y.moussawi@gmail.com>
2022-09-18 21:20:02 +0300

    organize and enhancement the code from sql injections

--- backend/api/display_user_info.php
+++ backend/api/display_user_info.php
@@ -1,18 +1,13 @@
 <?php
 include("../db/connection.php");
-//data will appear according to the id number of a specific user
 $id = $_GET['id'];
-//query to give the firstname,lastname and username 
-$query = $mysqli->prepare("SELECT first_name, last_name, username FROM users where id =" . $id);
-
+$query = $mysqli->prepare("SELECT first_name, last_name, username FROM users where id = ? limit 1");
+$query->bind_param('i', $id);
 $query->execute();
 $array = $query->get_result();
-
 $response = [];
-
 while ($a = $array->fetch_assoc()) {
     $response[] = $a;
 }
-
-$json = json_encode($response);
+$json = json_encode($response[0]);
 echo $json;
