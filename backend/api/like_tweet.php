7978a7cc23dd5fc3fd43f37584b2604620c10702
abbassmoussawi <abbass.y.moussawi@gmail.com>
2022-09-18 18:33:47 +0300

    'liken a tweet'

---
+++ backend/api/like_tweet.php
@@ -0,0 +1,17 @@
+<?php
+require('../db/connection.php');
+require('functions.php');
+
+//get the id's of users through POST method
+$user_follower = $_POST['user_follower'];
+$user_followed = $_POST['user_followed'];
+
+//create a query to allow the users follow each other 
+$query = $mysqli->prepare("INSERT INTO users_follow(user_follower,user_followed) VALUES (?,?)");
+$query->bind_param('ss', $user_follower,$user_followed);
+
+$query->execute();
+/*test the response*/
+$response = [];
+$response["success"] = true;
+echo json_encode($response);
