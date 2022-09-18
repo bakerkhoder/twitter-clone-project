8533a5e4f313377ca9b2299bc320ba186ce4b153
abbassmoussawi <abbass.y.moussawi@gmail.com>
2022-09-18 08:25:04 +0300

    'correct  a typo'

--- backend/api/likes_count.php
+++ backend/api/likes_count.php
@@ -2,7 +2,7 @@
 include("../db/connection.php");
 //data will appear according to the id number of a specific user
 $id = $_GET['id'];
-//this query gives the count of users who like a specific tweet 
+//this query gives the count of users who 'liked' a specific tweet 
 $query = $mysqli->prepare("
 SELECT COUNT (user_id) FROM likes_tweets WHERE tweet_id =" . $id);
 
