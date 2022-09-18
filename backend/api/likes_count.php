dbc74c783be75994b368077167771b590aa9ef56
abbassmoussawi <abbass.y.moussawi@gmail.com>
2022-09-18 21:25:41 +0300

    enhancement the code

--- backend/api/likes_count.php
+++ backend/api/likes_count.php
@@ -1,19 +1,11 @@
 <?php
 include("../db/connection.php");
-//data will appear according to the id number of a specific user
 $id = $_GET['id'];
-//this query gives the count of users who 'liked' a specific tweet 
-$query = $mysqli->prepare("
-SELECT COUNT (user_id) FROM likes_tweets WHERE tweet_id =" . $id);
-
+$query = $mysqli->prepare("SELECT COUNT(user_id) as count FROM likes_tweets WHERE tweet_id = ? ");
 $query->execute();
 $array = $query->get_result();
-
-$response = [];
-
-while ($a = $array->fetch_assoc()) {
-    $response[] = $a;
-}
-
-$json = json_encode($response);
-echo $json;
+$query->execute();
+$response = [
+    'success' => true
+];
+echo json_encode($response);
