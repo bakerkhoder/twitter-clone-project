1b5e3550504a623ced077a0e6b92a125cc6d428a
abbassmoussawi <abbass.y.moussawi@gmail.com>
2022-09-18 22:12:19 +0300

    insert image into database

--- backend/api/create_tweet.php
+++ backend/api/create_tweet.php
@@ -3,9 +3,10 @@ require('../db/connection.php');
 require('functions.php');
 $userId = check_input($_POST['user_id']);
 $content = check_input($_POST['content']);
+$url_image = check_input($_POST['tweet_image']);
 
-$query = $mysqli->prepare("INSERT INTO tweets(user_id,content) VALUES (?,?)");
-$query->bind_param('is', $userId, $content);
+$query = $mysqli->prepare("INSERT INTO tweets(user_id,content,tweet_image) VALUES (?,?,?)");
+$query->bind_param('iss', $userId, $content,$url_image);
 $query->execute();
 $response = [];
 $response["success"] = true;
