98f26da16cbf1243474eaf75089eaf7c8d3d2871
abbassmoussawi <abbass.y.moussawi@gmail.com>
2022-09-18 21:05:12 +0300

    enhancemnet the code

--- backend/api/create_tweet.php
+++ backend/api/create_tweet.php
@@ -1,18 +1,11 @@
 <?php
 require('../db/connection.php');
 require('functions.php');
+$userId = check_input($_POST['user_id']);
+$content = check_input($_POST['content']);
 
-//check_input():it removes space and special characters from data
-//in order to prevent the database from malicious data
-
- $userId =check_input($_POST['user_id']);
- $content = check_input($_POST['content']);
-
-
-// create a query to insert a tweet post into databse
 $query = $mysqli->prepare("INSERT INTO tweets(user_id,content) VALUES (?,?)");
-$query->bind_param('ss', $userId, $content);
-
+$query->bind_param('is', $userId, $content);
 $query->execute();
 $response = [];
 $response["success"] = true;
