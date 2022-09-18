cbf184c6f4d9e7ea97e75adfe18f26cf5f150f0c
abbassmoussawi <abbass.y.moussawi@gmail.com>
2022-09-18 21:27:35 +0300

    enhancement the code

--- backend/api/login.php
+++ backend/api/login.php
@@ -3,12 +3,9 @@ require('../db/connection.php');
 require('functions.php');
 $email =  $_POST['email'];
 $password = $_POST['password'];
-
-//create a query for login
 $query = $mysqli->prepare("SELECT * FROM users WHERE email = ? limit 1");
 $query->bind_param('s', $email);
 $query->execute();
-//create a variable array  to store the results
 $array = $query->get_result();
 
 $response = [];
@@ -19,14 +16,16 @@ while ($a = $array->fetch_assoc()) {
 
 if (empty($response)) {
     echo "operation failed";
+    exit;
 }
 
 $user = $response[0];
 
-//use password_verify() to check hshed password in database
+//use password_verify() to check the hashed password in database
 if (password_verify($password, $user['password'])) {
-    echo "login successed";
+    echo "login succeeded";
 }
 echo "login failed";
+exit;
 $json = json_encode($response);
 echo $json;
